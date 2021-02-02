fca9e7d53705e733424863e1106d96c209d2e431
Ram Iyengar <ramanujank@gmail.com>
2021-02-02 17:46:45 +0530

    add git clone and cd steps

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,5 +6,6 @@ cf auth "$INPUT_CF_USERNAME" "$INPUT_CF_PASSWORD"
 if [ -n "$INPUT_CF_ORG" ] && [ -n "$INPUT_CF_SPACE" ]; then
   cf target -o "$INPUT_CF_ORG" -s "$INPUT_CF_SPACE"
 fi
-
+git clone https://github.com/ramiyengar/test-app.git
+cd test-app
 sh -c "cf7 $*"
